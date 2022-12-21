db40e19d2576abd8dcb392953d684e4aa849eac1
Serkan Akman <akmanserkan65@gmail.com>
2022-12-22 00:06:15 +0300

    Upgrade to Next.js 13

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'avatar.tobi.sh',
+      'cloudflare-ipfs.com',
+      'loremflickr.com',
+      'vercel.com'
+    ]
+  },
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true
+  }
+};
+
+module.exports = nextConfig;
